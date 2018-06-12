533b3a2023cce40cf1f7c50889ecce1ba596fa06
Raphael Leber <raphael.leber@gmail.com>
2018-06-12 11:08:16 +0200

    openpose.launch

--- openpose_ros_examples/scripts/RosOpenPoseServiceExample.py
+++ openpose_ros_examples/scripts/RosOpenPoseServiceExample.py
@@ -29,11 +29,11 @@ def LoadImgAndPublish():
     rospy.init_node('LoadAndPublishImg', anonymous=True)
     
     #Load Image
-    img_loaded1 = cv2.imread(media_folder+'/pexels-photo-109919.jpeg')
-    msg_im1 = _bridge.cv2_to_imgmsg(img_loaded1, encoding="bgr8")
+    #img_loaded1 = cv2.imread(media_folder+'/pexels-photo-109919.jpeg')
+    #msg_im1 = _bridge.cv2_to_imgmsg(img_loaded1, encoding="bgr8")
 
-    img_loaded2 = cv2.imread(media_folder+'/pexels-photo.jpg')
-    msg_im2 = _bridge.cv2_to_imgmsg(img_loaded2, encoding="bgr8")
+    #img_loaded2 = cv2.imread(media_folder+'/pexels-photo.jpg')
+    #msg_im2 = _bridge.cv2_to_imgmsg(img_loaded2, encoding="bgr8")
 
     img_loaded3 = cv2.imread(media_folder+'/COCO_val2014_000000000544.jpg')
     msg_im3 = _bridge.cv2_to_imgmsg(img_loaded3, encoding="bgr8")
@@ -42,23 +42,23 @@ def LoadImgAndPublish():
     #call service to learn people
     rospy.wait_for_service('people_pose_from_img')
 
+    #try:
+    #    detect_from_img_srv = rospy.ServiceProxy('people_pose_from_img', DetectPeoplePoseFromImg)
+    #    resp1 = detect_from_img_srv(msg_im1)
+    #    print "nb people"
+    #    print "service:"+str(resp1.personList)
+    #except rospy.ServiceException, e:
+    #    print "Service call failed: %s"%e
+    
+    #try:
+    #    resp2 = detect_from_img_srv(msg_im2)
+    #    print "nb people"
+    #    print "service:"+str(resp2.personList)
+    #except rospy.ServiceException, e:
+    #    print "Service call failed: %s"%e
+
     try:
         detect_from_img_srv = rospy.ServiceProxy('people_pose_from_img', DetectPeoplePoseFromImg)
-        resp1 = detect_from_img_srv(msg_im1)
-        print "nb people"
-        print "service:"+str(resp1.personList)
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
-    
-    try:
-        resp2 = detect_from_img_srv(msg_im2)
-        print "nb people"
-        print "service:"+str(resp2.personList)
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
-
-
-    try:
         resp3 = detect_from_img_srv(msg_im3)
         print "nb people"
         print "service:"+str(resp3.personList)
